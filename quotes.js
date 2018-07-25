14155f2d6c67e9625c6f6109e531974ce144ae6c
Josh Harris <me@celant.co.uk>
2018-07-25 16:31:21 +0100

    Updated quotes

--- quotes.js
+++ quotes.js
@@ -26,13 +26,140 @@ class quotesModule {
 		self.commands.addCommand({
 			trigger: "!addquote",
 			function: self.addQuoteCommand,
-			permCheck: self.perms.pass
+			permCheck: self.perms.pass,
+			thisarg: this
 		});
 		self.commands.addCommand({
-			trigger: "!roll",
-			function: self.rollCommand,
-			permCheck: self.perms.pass
+			trigger: "!delquote",
+			function: self.delQuoteCommand,
+			permCheck: self.perms.isInAdminRoom,
+			thisarg: this
 		});
+	}
+
+
+	addQuote(quote, msg, callback) {
+		var self = this;
+
+		return new Promise(function(resolve, reject) {
+
+			var sqlProps = [
+				quote.content,
+				quote.author.id,
+				quote.createdAt,
+				quote.channel.id,
+				msg.author.id,
+				quote.guild.id
+			];
+
+			self.bot.mysql.query("INSERT INTO quotes (`content`, `authorid`, `timestamp`, `channel`, `addedbyid`, `guildid`) VALUES (?,?,?,?,?,?)", sqlProps, function(err, res) {
+				if (err) {
+					reject(err);
+					return
+				}
+
+				resolve(res.insertId);
+			});
+		});
+	}
+
+	delQuote(quoteId, callback) {
+		var self = this;
+
+		return new Promise(function(resolve, reject) {
+
+			self.bot.mysql.query("DELETE FROM quotes WHERE id=?", quoteId, function(err, res) {
+				if (err) {
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+		});
+	}
+
+	checkQuoteExists(quoteId, callback) {
+		var self = this;
+
+		return new Promise(function(resolve, reject) {
+			self.bot.mysql.query("SELECT COUNT(id) AS count FROM quotes WHERE id=?", quoteId, function(err, res) {
+				if (err) {
+					reject(err);
+					return;
+				}
+
+				if (res[0].count === 1) {
+					resolve(true);
+				} else {
+					resolve(false);
+				}
+			});
+		});
+	}
+
+	fetchQuote(quoteId, fromUser, context) {
+		return new Promise(function(resolve, reject) {
+			let query = "";
+			let queryOpts = [];
+
+			if (quoteId) {
+				query = "SELECT *, DATE_FORMAT(timestamp,\"%Y-%m-%dT%TZ\") AS timestamp FROM quotes WHERE id=?";
+			} else {
+				if (fromUser) {
+					query = "SELECT *, DATE_FORMAT(timestamp, \"%Y-%m-%dT%TZ\") AS timestamp FROM quotes WHERE authorid=? ORDER BY RAND() LIMIT 1";
+					queryOpts.push(fromUser.id);
+				} else {
+					query = "SELECT *, DATE_FORMAT(timestamp, \"%Y-%m-%dT%TZ\") AS timestamp FROM quotes ORDER BY RAND() LIMIT 1";
+				}
+			}
+
+			self.bot.mysql.query(query, queryOpts, function(err, res) {
+				if (err) {
+					reject(err);
+					return;
+				}
+
+				var quote = res[0];
+
+				if (quote) {
+					resolve(self.formatQuote(quote, context));
+				} else {
+					reject();
+				}
+			});
+		});
+	}
+
+	formatQuote(quote, context) {
+		var self = this;
+
+		var member = context.guild.members.get(quote.authorid);
+		var channel = context.guild.channels.get(quote.channel);
+		if (member === undefined) {
+			member.displayName = quote.authorid;
+			member.user = {};
+			member.user.displayAvatarURL = function() { return "https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png" }
+		}
+		if (channel === undefined) {
+			channel = {}
+			channel.name = "Unknown Channel";
+		}
+
+		var embed = {
+			color: 16035906,
+			timestamp: quote.timestamp,
+			author: {
+				name: member.displayName,
+				icon_url: member.user.displayAvatarURL()
+			},
+			description: quote.content,
+			footer: {
+				text: "In " + channel.name
+			}
+		}
+
+		return embed;
 	}
 
 	addQuoteCommand(msg) {
@@ -41,32 +168,27 @@ class quotesModule {
 		if (msg.parts[1]) {
 			if (/^\d{5,25}$/.test(msg.parts[1])) {
 				var messageId = msg.parts[1];
-				guild.search({
-					minID: messageId,
-					maxID: messageId,
-					contextSize: 0,
-					limit: 1
-				}).then(res => {
-					const hit = res.results[0].find(m => m.hit);
 
-					if (hit) {
-						addQuoteToDatabase(quote, msg, function(err, quoteId) {
-							var quote = fetchQuote(quoteId, msg, function(err, quote) {
-								if (err) {
-									msg.reply("looks like the quote didn't save properly in my database :slight_frown: :disagree:");
-									return;
-								}
+				var messagePromise = msg.channel.messages.fetch({around: messageId, limit: 1});
 
-								msg.reply("added as quote " + quoteId + " :agree:", {
-									embed: quote
-								});
-							});	
+				messagePromise.then(function(messages) {
+					var quote = messages.first();
+					self.addQuote(quote, msg).then(function(quoteId) {
+						self.fetchQuote(quoteId, msg).then(function(quote) {
+							msg.reply("added as quote " + quoteId + " :white_check_mark:", {
+								embed: quote
+							});
+						}).catch(function(err) {
+							console.log(err);
+							msg.reply("looks like the quote got stuck somewhere! :slight_frown:");
 						});
-					} else {
-						msg.reply("I'm afraid I couldn't find that message, double check it's ID, and that it still exists!");
-					}
-				}).catch(err => {
-					msg.reply("a fatal error happened while trying to find that meassage :slight_frown:");
+					}).catch(function(err) {
+						console.log(err);
+						msg.reply("looks like the quote didn't save properly in my database :slight_frown:");
+					});
+				}).catch(function(err) {
+					console.log(err);
+					msg.reply("I couldn't find that message. Is the ID correct, or has the message been deleted? :confounded:");
 				});
 			}
 		} else {
@@ -74,73 +196,60 @@ class quotesModule {
 		}
 	}
 
-	addQuoteToDatabase(quote, msg, callback) {
-		var sqlProps = [
-			quote.content,
-			quote.author.id,
-			quote.channel.id,
-			msg.author.id,
-			quote.guild.id
-		];
+	delQuoteCommand(msg) {
+		var self = this;
 
-		self.bot.mysql.query("INSET INTO quotes (`content`, `authorid`, `timestamp`, `channel`, `addedbyid`, `guildid`) VALUES (?,?,?,NOW(),?,?)", sqlProps, function(err, res) {
-			if (err) {
-				console.log(err);
-				callback(err, null);
-			}
+		var quoteId = msg.parts[1];
 
-			callback(null, res.insertId);
-		});
-	}
-
-	fetchQuote(quoteId, msg, callback) {
-		self.bot.mysql.query("SELECT *, DATE_FORMAT(timestamp,"%Y-%m-%dT%TZ") AS timestamp FROM quotes WHERE id=?", [quoteId], function(err, res) {
-			if (err) {
-				console.log(err);
-				callback(err, null);
-			}
-
-			var quote = res[0];
-
-			msg.guild.members.fetch(quote.authorid)
-			.then(member => {
-				var embed = {
-					color: 16035906,
-					timestamp: quote.timestamp,
-					author: {
-						name: member.displayName,
-						icon_url: member.user.displayAvatarURL()
-					},
-					description: quote.content,
-					footer: {
-						text: "In #" + quote.channel
-					}
+		if (quoteId) {
+			self.checkQuoteExists(quoteId).then(function(exists) {
+				if (!exists) {
+					msg.reply("that quote does not exist!");
+					return;
 				}
 
-				callback(null, embed);
+				self.delQuote(msg.parts[1]).catch(function(err) {
+					console.log(err);
+					msg.reply("internal error occured :slight_frown:");
+				});
 
+			}).catch(function(err) {
+				console.log(err);
+				msg.reply("internal error occured :slight_frown:");
+				return;
 			});
-
 		}
 	}
 
-	punCommand(msg) {
+	quoteCommand(msg) {
 		var self = this;
 
-		self.bot.mysql.query("INSERT INTO puns (`submitter`, `added`) VALUES (?, NOW())", [msg.author.username], function(err, res){
-			if (err) {
+		let mentions = msg.orderedMentions();
+
+		if (mentions[0]) {
+			self.fetchQuote(null, mentions[0], msg).then(function(quote) {
+				msg.reply("", { embed: quote });
+			}).catch(function(err) {
 				console.log(err);
+				msg.reply("I couldn't find any quotes of that user :thinking:");
+			});
+		} else {
+			if (msg.parts[1] && /^\d{1,6}$/.test(msg.parts[1])) {
+				self.fetchQuote(msg.parts[1], null, msg).then(function(quote) {
+
+				}).catch(function(err) {
+					console.log(err);
+					msg.reply("I couldn't find that quote :thinking:");
+				});
+			} else {
+				self.fetchQuote(null, null, msg).then(function(quote) {
+
+				}).catch(function(err) {
+					console.log(err);
+					msg.reply("I couldn't find any quotes :thinking:");
+				});
 			}
-		});
-
-		self.bot.mysql.query("SELECT COUNT(*) AS count FROM puns", function(err, res) {
-                        if (err) {
-                                throw err;
-                        }
-                        var total = res[0].count;
-
-			msg.reply("You incremented the pun counter, there has been " + total + " terrible puns! :cactus:");
-                });
+		}
 	}
 }
 module.exports = quotesModule;
